190ad5a04bba79365c6c792959f9a4071ccf5a4a
madhuragouda <madhura.gouda.4work@gmail.com>
2024-02-28 11:42:45 +0530

    add new react file

---
+++ axios/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+ import Featch from './Featch'
+// import GridT from './GridT'
+
+function App() {
+  return (
+   
+    <Featch/>
+    // <GridT/>
+  )
+}
+
+export default App
+// import { useState,useEffect } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+// import axios from'axios';
+// import './App.css'
+
+// function App() {
+//    const [myData, setMyData] = useState([])
+//   useEffect(()=>{
+//     axios
+//     .get('https://jsonplaceholder.typicode.com/posts/')
+//     .then((res)=>setMyData(res.data));
+   
+
+//   },[]);
+
+//   return (
+//     <>
+//     <h1>Axios Tutorials</h1>
+//     <div className='grid'>
+//     {myData.map((post)=>{
+//       const{id, title,body}=post;
+//       return (
+//       <div className='card' key={id}>
+//         <h2>{title.slice(0, 15).toUpperCase()}</h2>
+//         <p>{body.slice}</p>
+      
+
+//       </div>
+//     )
+//         })}
+//     </div>
+//     </>
+    
+//   )
+// }
+
+// export default App
---
+++ axios/src/Featch.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import'./App.css'
+
+function Featch() {
+    const[data,setData]=useState([])
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/posts/')
+        .then(res=>setData(res.data))
+        .catch(err=>console.log(err));
+    },[])
+  return (
+    <div className='container'>
+        <div className='mt-3'>
+            <h3> Fetch data from API in react with axios</h3>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th> title</th>
+                        <th>body</th>
+                        {/* <th> email</th> */}
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((user, index)=>{
+                            return <tr key={index}>
+                                <td>{user.id}</td>
+                                <td>{user.title}</td>
+                                <td>{user.body}</td>
+                                {/* <td>{user.email}</td> */}
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    </div>
+  )
+}
+
+export default Featch
